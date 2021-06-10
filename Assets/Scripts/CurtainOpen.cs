7b7f575618a1468cadb07da9b075450f68366696
unknown <eda.koyuncu.22@gmail.com>
2021-06-10 16:18:57 +0200

    added curtain animation and added UITests Folder in Scenes

---
+++ Assets/Scripts/CurtainOpen.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class CurtainOpen : MonoBehaviour
+{
+    Animator anim;
+    bool transition = true;
+    float closedTimer;
+    [SerializeField] float transitionTime = 5f;
+    
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("open", transition);
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+            closedTimer -= Time.deltaTime;
+            if(closedTimer<= 0)
+            {
+                anim.SetBool("open", transition);
+                closedTimer = transitionTime;
+                transition = !transition;
+            }
+        
+        
+
+    }
+}
